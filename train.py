1d43e33293599c3f7f06e6f6cf856cdd00d836f2
silence.sun <silence.sun@paytm.com>
2023-05-10 11:24:11 +0800

    update train.py

--- train.py
+++ train.py
@@ -1,9 +1,18 @@
 from ultralytics import YOLO
 
+
+
+
 # Load a model
-model = YOLO(model='yolov8.yaml')  # build a new model from YAML
+# model = YOLO(model='yolov8.yaml')  # build a new model from YAML
+# 加载上次训练的模型，继续训练 填写上次训练的 /content/drive/MyDrive/datafountain-552/workspace/datafountain552/runs/detect/train7/weight/last.pt
+model = YOLO(model='/content/drive/MyDrive/datafountain-552/workspace/datafountain552/runs/detect/train7/weight/last.pt')
+
+
 # model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 # model = YOLO(model='./dataset/df.yaml', task='detect').load('./models/yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
 model.train(data='./dataset.yaml', epochs=100, batch=4, imgsz=2600)
+
+

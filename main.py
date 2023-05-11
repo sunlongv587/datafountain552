4c50727df166dde3be5d71e95d487d99c515ed56
silence.sun <silence.sun@paytm.com>
2023-05-11 16:17:11 +0800

    update main

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
-# This is a sample Python script.
+# lena
+import cv2
+import os
+from ultralytics import YOLO
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Load the YOLOv8 model n=微小的 pt = pytorch
+# model = YOLO('yolov8n.pt')  # load an official model
+model = YOLO(model='./weights/best.pt')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# # Validate the model
+# metrics = model.val()  # no arguments needed, dataset and settings remembered
+# metrics.box.map    # map50-95
+# metrics.box.map50  # map50
+# metrics.box.map75  # map75
+# metrics.box.maps   # a list contains map50-95 of each category
+
+
+def image_detect():
+    test_dir = os.path.expanduser("~/Notes/Datasets/person_and_car_dataset_from_datafountain/test_images")
+    img_list = os.listdir(test_dir)
+    for img in img_list:
+        img_path = os.path.join(test_dir, img)
+        # Predict with the model
+        results = model(source=img_path, show=False, save=True)  # predict on an image
+        annotated_frame = results[0].plot()
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+        key = cv2.waitKey(0)
+        if key == 113:
+            break
+    cv2.destroyAllWindows()
+
+
+# Open the video file
+# video_path = "path/to/your/video/file.mp4"
+def video_detect():
+    cap = cv2.VideoCapture(0)
+
+    # Loop through the video frames
+    while cap.isOpened():
+        # Read a frame from the video
+        res, frame = cap.read()
+
+        if res:
+            # Run YOLOv8 inference on the frame
+            results = model(frame)
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # Display the annotated frame
+            cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            # Break the loop if the end of the video is reached
+            break
+
+    # Release the video capture object and close the display window
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # video_detect()
+    image_detect()
--- train.py
+++ train.py
@@ -7,14 +7,40 @@ import os
 # model = YOLO(model='yolov8.yaml')  # build a new model from YAML
 # 加载上次训练的模型，继续训练 填写上次训练的 /content/drive/MyDrive/datafountain-552/workspace/datafountain552/runs/detect/train7/weight/last.pt
 
-last_model_path = '/content/drive/MyDrive/datafountain-552/workspace/datafountain552/runs/detect/train0002/weights/last.pt'
-print(f"上次训练模型 last.pt 路径是：{last_model_path}")
-model = YOLO(model=last_model_path)
+def find_last_model(runs_path):
+    # train_dir = '/content/drive/MyDrive/datafountain-552/workspace/datafountain552/runs/detect/train0002/weights/last.pt'
+    detect_dir = 'detect'
+    detect_path = os.path.join(runs_path, detect_dir)
+    folders = []
+    # 读取detect目录下所有文件夹信息
+    for entry in os.scandir(detect_path):
+        if entry.is_dir():
+            folders.append(entry)
 
-# model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-# model = YOLO(model='./dataset/df.yaml', task='detect').load('./models/yolov8n.pt')  # build from YAML and transfer weights
+    # 按创建时间倒序排列所有文件夹
+    sorted_folders = sorted(folders, key=lambda e: e.stat().st_ctime, reverse=True)
 
-# Train the model
-model.train(data='./dataset.yaml', epochs=100, batch=4, imgsz=2600)
+    # 打印所有文件夹名
+    for folder in sorted_folders:
+        last_model_path = os.path.join(detect_path, folder.name, 'weights', 'last.pt')
+        if os.path.isfile(last_model_path):
+            print(f"上次训练的文件找到，位置：{last_model_path}")
+            return last_model_path
+        else:
+            print(f"文件不存在：{last_model_path}, 继续查找上次训练的模型")
 
+
+def main():
+    # last_model_path = '/content/drive/MyDrive/datafountain-552/workspace/datafountain552/runs/detect/train0002/weights/last.pt'
+    last_model_path = find_last_model("/content/drive/MyDrive/datafountain-552/workspace/datafountain552/runs")
+    if last_model_path == None:
+        last_model_path = "yolov8n.yaml"
+    print(f"加载上次训练的模型，last.pt 路径是：{last_model_path}")
+    model = YOLO(model=last_model_path)
+
+    # Train the model
+    model.train(data='./dataset.yaml', epochs=100, batch=4, imgsz=2600)
+
+if __name__ == '__main__':
+    main()
 
